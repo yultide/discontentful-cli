935f240b94677327008ab98fe0a5a81c11724589
Yulius Tjahjadi <yultide@yahoo.com>
2025-02-20 11:31:09 -0800

    fix: add coverage config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,12 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	test: {},
+	test: {
+		coverage: {
+			// you can include other reporters, but 'json-summary' is required, json is recommended
+			reporter: ['text', 'json-summary', 'json'],
+			// If you want a coverage reports even if your tests are failing, include the reportOnFailure option
+			reportOnFailure: true,
+		},
+	},
 });
